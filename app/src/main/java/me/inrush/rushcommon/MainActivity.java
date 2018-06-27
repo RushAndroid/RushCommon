ccfe0bf8db2b1c15855a7ec9c8453b6e2636d757
inRush <1289110832@qq.com>
2018-06-27 13:31:45 +0800

    add test demo and use qmui library as base ui

--- app/src/main/java/me/inrush/rushcommon/MainActivity.java
+++ app/src/main/java/me/inrush/rushcommon/MainActivity.java
@@ -1,16 +1,16 @@
 package me.inrush.rushcommon;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import me.inrush.common.app.BaseActivity;
 
 /**
  * @author inrush
  */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected int getContentLayoutId() {
+        return R.layout.activity_main;
     }
+
+
 }
