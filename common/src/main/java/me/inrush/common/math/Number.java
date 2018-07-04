529a8cab538953c4fdcb4814efea50476beead28
inRush <1289110832@qq.com>
2018-07-04 15:56:50 +0800

    add Number Utils class，but not impl

---
+++ common/src/main/java/me/inrush/common/math/Number.java
@@ -0,0 +1,20 @@
+package me.inrush.common.math;
+
+/**
+ * @author inrush
+ * @date 2018/6/30 下午8:04
+ */
+public class Number {
+    private byte[] mNumber;
+
+    public Number(String number){
+
+    }
+    public Number(int number){
+
+    }
+    public Number(double number){
+
+    }
+
+}
